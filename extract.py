4278c5302c01edfe344ed3bcaea2bac782f35bc7
Philipp <philipp.wiesenbach@uni-heidelberg.de>
2025-02-14 15:01:47 +0100

    Minor fix and script to calculate token count.

--- extract.py
+++ extract.py
@@ -17,63 +17,68 @@ from prompts import prompts, system_prompt
 
 texts = docs if args.doclevel == "docs" else chunks
 
-print(f"New run: {triple_pkl_path.parent}")
+# print(f"New run: {triple_pkl_path.parent}")
 
 if args.extractionmode == "nerrel":
     ner_prompt = prompts.pop(0)
 
 with open(triple_pkl_path, "wb") as triple_pkl_file:
     for doc in texts:
-        text = doc[0].page_content
-        messages: list[Message] = []
-        responses = list()
-        # while 1:
-        # try:
-        if args.extractionmode == "nerrel":
-            message = Message(role="user", content=ner_prompt)
-            messages.append(message)
-            if not args.all_ners_given:
-                response = b.ExtractNEs(
+        try:
+            text = doc[0].page_content
+            messages: list[Message] = []
+            responses = list()
+            # while 1:
+            # try:
+            if args.extractionmode == "nerrel":
+                message = Message(role="user", content=ner_prompt)
+                messages.append(message)
+                if not args.all_ners_given:
+                    response = b.ExtractNEs(
+                        system_prompt,
+                        text,
+                        message,
+                        {"client_registry": cr},
+                    )
+                    responses.append(response)
+                    messages.append(
+                        Message(
+                            role="assistant", content=f" ENTITY LIST: {str(response)}"
+                        )
+                    )
+                else:
+                    ner_path = [
+                        x
+                        for x in all_ner_paths
+                        if doc[0].metadata["file_path"].stem == x.stem
+                    ][0]
+                    if ner_path:
+                        ners = open(ner_path, "r").readlines()
+                        ners = [x.strip() for x in ners]
+                    else:
+                        ners = []
+                    responses.append(ners)
+                    messages.append(
+                        Message(role="assistant", content=f" ENTITIY LIST: {str(ners)}")
+                    )
+            for i, prompt in enumerate(prompts):
+                messages.append(Message(role="user", content=prompt))
+                response = b.GeneralChatExtractRelationships(
                     system_prompt,
                     text,
-                    message,
+                    messages,
                     {"client_registry": cr},
                 )
                 responses.append(response)
-                messages.append(
-                    Message(role="assistant", content=f" ENTITY LIST: {str(response)}")
-                )
-            else:
-                ner_path = [
-                    x
-                    for x in all_ner_paths
-                    if doc[0].metadata["file_path"].stem == x.stem
-                ][0]
-                if ner_path:
-                    ners = open(ner_path, "r").readlines()
-                    ners = [x.strip() for x in ners]
-                else:
-                    ners = []
-                responses.append(ners)
-                messages.append(
-                    Message(role="assistant", content=f" ENTITIY LIST: {str(ners)}")
-                )
-        for i, prompt in enumerate(prompts):
-            messages.append(Message(role="user", content=prompt))
-            response = b.GeneralChatExtractRelationships(
-                system_prompt,
-                text,
-                messages,
-                {"client_registry": cr},
-            )
-            responses.append(response)
-            messages.append(Message(role="assistant", content=str(response)))
-            print(f"STEP {i}, {response}")
+                messages.append(Message(role="assistant", content=str(response)))
+                # print(f"STEP {i}, {response}")
 
-        pickle.dump(
-            (responses, doc[0].page_content, doc[0].metadata["file_path"]),
-            triple_pkl_file,
-        )
+            pickle.dump(
+                (responses, doc[0].page_content, doc[0].metadata["file_path"]),
+                triple_pkl_file,
+            )
+        except:
+            pass
 
         if args.dev:
             break
